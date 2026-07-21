ca08b8f21a0e1bf3efe702fad03aee1c649ba417
Nagender kumar <kumarnagender811@gmail.com>
2026-07-21 21:19:20 +0530

    Add initial CrowdFunding contract

---
+++ test/CrowdFunding.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {CrowdFunding} from "../src/CrowdFunding.sol";
+
+contract CrowdFundingTest is Test {
+   
+}
